e39ade9a3ada54bb909c689454dee9c808fd6de3
Hakan Akyuz <legion61614@gmail.com>
2024-09-22 23:17:46 +0300

    __left side view bug fixing

--- client/src/components/LeftSide/LeftSide.jsx
+++ client/src/components/LeftSide/LeftSide.jsx
@@ -30,53 +30,60 @@ const LeftSide = () => {
     }, [isCreateFieldVisible])
 
     return (
-        <div className={`left-side-container ${isMessagePage ? 'left-side-container-message' : ''}`}>
-            <div className={`left-side-header ${isMessagePage ? 'left-side-header-message' : ''}`}>
-                <Link to={`/`} className={``}>
-                    <RxVercelLogo/>
-                </Link>
-                <div className='more-when-small-screen nav-container' onClick={handleToggleMore}>
-                    <RxHamburgerMenu/>
-                </div>
-            </div>
-            <div className='left-inner-side-container'>
-                <Link to={`/`} className={`nav-logo-container ${isMessagePage ? 'nav-logo-container-message' : ''}`}>
-                <span>Can't name it yet!</span>
-                    <RxVercelLogo />
-                </Link>
-                <Link to={`/`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
-                    <GoHome/>
-                    <span>Home</span>
-                </Link>
-                <Link to={`/notifications`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
-                    <IoIosNotificationsOutline className='notification-element'/>
-                    <span>Notifications</span>
-                </Link>
-                <Link to={`/search`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
-                    <IoSearchOutline/>
-                    <span>Search</span>
-                </Link>
-                <Link to={`/messages`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
-                    <BiMessageSquareDetail/>
-                    <span>Messages</span>
-                </Link>
-                <div className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`} onClick={handleToggleCreateField}>
-                    <IoCreateOutline/>
-                    <span>Create</span>
-                </div>
-                <div className='left-side-profile-picture-container'>
-                    <Link to={`/profile`} className='left-side-profile-picture'>
-                        PP
+        <div className='left-side-outer'>
+            <div className={`left-side-container ${isMessagePage ? 'left-side-container-message' : ''}`}>
+                <div className={`left-side-header ${isMessagePage ? 'left-side-header-message' : ''}`}>
+                    <Link to={`/`} className={``}>
+                        <RxVercelLogo/>
                     </Link>
+                    <div className='more-when-small-screen nav-container' onClick={handleToggleMore}>
+                        <RxHamburgerMenu/>
+                    </div>
                 </div>
-                {isCreateFieldVisible && <CreatePost closeModal={handleToggleCreateField} />}
+                <div className='left-inner-side-container'>
+                    <Link to={`/`}
+                          className={`nav-logo-container ${isMessagePage ? 'nav-logo-container-message' : ''}`}>
+                        <span>Can't name it yet!</span>
+                        <RxVercelLogo/>
+                    </Link>
+                    <Link to={`/`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
+                        <GoHome/>
+                        <span>Home</span>
+                    </Link>
+                    <Link to={`/notifications`}
+                          className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
+                        <IoIosNotificationsOutline className='notification-element'/>
+                        <span>Notifications</span>
+                    </Link>
+                    <Link to={`/search`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
+                        <IoSearchOutline/>
+                        <span>Search</span>
+                    </Link>
+                    <Link to={`/messages`} className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}>
+                        <BiMessageSquareDetail/>
+                        <span>Messages</span>
+                    </Link>
+                    <div className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}
+                         onClick={handleToggleCreateField}>
+                        <IoCreateOutline/>
+                        <span>Create</span>
+                    </div>
+                    <div className={`left-side-profile-picture-container ${isMessagePage ? 'left-side-profile-picture-container-message' : ''}`}>
+                        <Link to={`/profile`} className='left-side-profile-picture'>
+                            PP
+                        </Link>
+                    </div>
+                    {isCreateFieldVisible && <CreatePost closeModal={handleToggleCreateField}/>}
+                </div>
+                <div className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`}
+                     onClick={handleToggleMore}>
+                    <RxHamburgerMenu/>
+                    <span>More</span>
+                </div>
+                {isMoreVisible && <More closeModal={handleToggleMore}/>}
             </div>
-            <div className={`nav-container ${isMessagePage ? 'nav-container-message' : ''}`} onClick={handleToggleMore}>
-                <RxHamburgerMenu/>
-                <span>More</span>
-            </div>
-            {isMoreVisible && <More closeModal={handleToggleMore} />}
         </div>
+
     )
 }
 
